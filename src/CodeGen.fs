0cb6b623ffc86895b8cd9936c13cc1c1a0b37892
Christian <Christianjohansen@yahoo.dk>
2018-02-16 12:28:34 +0100

    Boolean literals

--- src/CodeGen.fs
+++ src/CodeGen.fs
@@ -172,9 +172,10 @@ let rec compileExp  (e      : TypedExp)
       else
         [ Mips.LUI (place, makeConst (n / 65536))
         ; Mips.ORI (place, place, makeConst (n % 65536)) ]
-  | Constant (BoolVal _, _) ->
+  | Constant (BoolVal b, pos) ->
+      if b then [ Mips.LI (place, makeConst (int 1)) ]
+      else [ Mips.LI (place, makeConst (int 0)) ]
       (* TODO project task 1: represent `true`/`false` values as `1`/`0` *)
-      failwith "Unimplemented code generation of boolean constants"
   | Constant (CharVal c, pos) -> [ Mips.LI (place, makeConst (int c)) ]
 
   (* Create/return a label here, collect all string literals of the program
